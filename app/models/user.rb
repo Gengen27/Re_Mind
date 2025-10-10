2209d902b3370de266e9408843924f592a516055
Gengen27 <tachibana7227@gmail.com>
2025-10-10 15:11:27 +0900

    feat:Userモデルの作成
    deviseの認証機能とその他のテーブルとの関連付け。 またそれらのバリデーションの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,11 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable
+
+  has_many :posts, dependent: :destroy
+  
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :mentor_personality, inclusion: { in: %w[gentle strict logical balanced] }
+  
 end
