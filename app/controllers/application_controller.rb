af9ae73c9eac91fe10a07714a02b886268382d2c
Gengen27 <tachibana7227@gmail.com>
2025-10-21 18:23:28 +0900

    feat:デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  # 本番環境でのみベーシック認証を有効化
+  before_action :basic_auth, if: :production?
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :set_pending_reminders_count
@@ -15,6 +17,18 @@ class ApplicationController < ActionController::Base
       @pending_reminders_count = current_user.reminders
                                              .where(status: ['ready', 'in_progress'])
                                              .count
+    end
+  end
+  
+  private
+  
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
